830428d8476554e366003bf107d690418fbdf706
hantaiyan <53936838+hantaiyan@users.noreply.github.com>
2025-11-29 15:44:11 +0800

    Update extract_primary2sqlite.py

--- extract_codes/extract_primary2sqlite.py
+++ extract_codes/extract_primary2sqlite.py
@@ -2,14 +2,12 @@ import pyarrow.dataset as ds
 import sqlite3
 from tqdm import tqdm
 
-# ========== 配置路径 ==========
 # parquet_path = "/Users/zhaoxinhang/openalex-primary.parquet"
 # sqlite_path = "/Users/zhaoxinhang/openalex-primary.sqlite"
 
 parquet_path = "/Users/zhaoxinhang/N/openalex-primary.parquet"
 sqlite_path = "/Users/zhaoxinhang/N/openalex-primary.sqlite"
 
-# ========== 初始化 SQLite ==========
 conn = sqlite3.connect(sqlite_path)
 cursor = conn.cursor()
 
@@ -25,12 +23,10 @@ CREATE TABLE IF NOT EXISTS primary_source (
 """)
 conn.commit()
 
-# ========== 使用 scanner 分批读取 ==========
 dataset = ds.dataset(parquet_path, format="parquet")
 scanner = dataset.scanner(columns=["id", "publication_date", "source_id", "source_type", "source_name", "issn_l"])
 
-print("🚀 开始分批写入 SQLite...")
-for batch in tqdm(scanner.to_batches(), desc="📦 插入中"):
+for batch in tqdm(scanner.to_batches(), desc="insert"):
     df = batch.to_pandas()
     values = df.values.tolist()
 
@@ -42,4 +38,3 @@ for batch in tqdm(scanner.to_batches(), desc="📦 插入中"):
     conn.commit()
 
 conn.close()
-print("✅ 所有数据已成功写入 SQLite！")
